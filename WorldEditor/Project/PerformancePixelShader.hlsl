41e81a590a6b07871b54af5d8439466ac0b73959
PhilipBerglund <philipberglund1@hotmail.com>
2022-09-20 17:43:29 +0200

    Update PerformancePixelShader.hlsl
    Interpolation addef for the Performance Shader

--- WorldEditor/Project/PerformancePixelShader.hlsl
+++ WorldEditor/Project/PerformancePixelShader.hlsl
@@ -19,9 +19,27 @@ float4 main(PS_INPUT input) : SV_TARGET
     float4 NOT = float4(1.0f, 0.0f, 0.0f, 1.0f);
     float4 color = NOT;
 
-    if (modeltime < limit)
-    {
-        color = OK;
-    }
+    float rangeMin = 0.0f;
+    float rangeMax = 1.0f;
+    float oldRangeMin = limit * 0.2f;
+    float oldRangeMax = limit;
+    
+    float oldRange = (oldRangeMax - oldRangeMin);
+    float newRange = (rangeMax - rangeMin);
+    float amount = 0.0f;
+    
+    //amount = (((modeltime - oldRangeMin) * newRange) / oldRange) + rangeMin;
+    
+    amount = (((modeltime - oldRangeMin) * (rangeMax - rangeMin)) / (oldRangeMax - oldRangeMin)) + rangeMin;
+    float4 amountCol = float4(amount, 0.0f, amount, 0.0f);
+    
+    //if (modeltime < limit)
+    //{
+    //    color = OK;
+    //}
+    
+
+    color = lerp(OK, NOT, amountCol);
+    
     return color;
 }
